cea6bc3bb5c5650bb9dc2459dee683dc8eeae32d
LuisOcasio <ocasio.dev@icloud.com>
2021-04-25 18:28:56 +0000

    package install

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Amplify from "aws-amplify";
-import config from "./aws-exports";
+import config from "./aws-exports.js";
 Amplify.configure(config);
 
 ReactDOM.render(
